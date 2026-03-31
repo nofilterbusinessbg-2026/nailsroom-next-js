63521aaa30797ce436d0fbafc56e61d0c0319c5f
nofilterbusinessbg-2026 <nofilterbusinessbg@gmail.com>
2026-03-31 22:07:17 +0300

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,47 +1,11 @@
-import { readFile } from "node:fs/promises";
-import path from "node:path";
-import Script from "next/script";
-
-async function loadOriginalHtmlParts() {
-  const filePath = path.join(process.cwd(), "public", "original.html");
-  const originalHtml = await readFile(filePath, "utf8");
-
-  const styleMatch = originalHtml.match(/<style>([\s\S]*?)<\/style>/i);
-  const bodyMatch = originalHtml.match(/<body[^>]*>([\s\S]*?)<\/body>/i);
-
-  if (!styleMatch || !bodyMatch) {
-    throw new Error("Could not parse original.html content.");
-  }
-
-  const scriptMatch = bodyMatch[1].match(/<script[^>]*>([\s\S]*?)<\/script>/i);
-  const bodyWithoutScript = bodyMatch[1].replace(/<script[\s\S]*?<\/script>/gi, "");
-
-  return {
-    styles: styleMatch[1],
-    body: bodyWithoutScript,
-    script: scriptMatch ? scriptMatch[1] : "",
-  };
-}
-
-export default async function Home() {
-  const { styles, body, script } = await loadOriginalHtmlParts();
-
+export default function Home() {
   return (
-    <>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,600;1,300;1,400&family=Jost:wght@300;400;500&display=swap"
+    <main style={{ width: "100%", height: "100vh", margin: 0 }}>
+      <iframe
+        src="/original.html"
+        title="Nails Room"
+        style={{ width: "100%", height: "100%", border: 0, display: "block" }}
       />
-      <style dangerouslySetInnerHTML={{ __html: styles }} />
-      <div dangerouslySetInnerHTML={{ __html: body }} />
-      {script ? (
-        <Script
-          id="original-inline-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{ __html: script }}
-        />
-      ) : null}
-    </>
+    </main>
   );
 }
